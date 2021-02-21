5fc17bc9b595dc09f23dd3e2e0047101d7e4900e
Danila Olesyuk <olesyukdanila122@gmail.com>
2021-02-21 19:18:35 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+	if (array.length == 0 || Array.isArray(array) == false  || arguments.length == 0 ) {
+		return 0
+	}
+	
+	let newArray = array.sort((a,b)=> a - b);
+	
+	return newArray[0]
+
+	 
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+	if (array == [] || arguments.length == 0 ) {
+		return 0
+	}
+	
+	let newArray = array.sort((a,b)=> a - b);
+	
+	return newArray[array.length - 1]
+
+	 
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+	if (array.length == 0 || arguments.length == 0) {
+		return 0
+		
+	}
+
+	let mewArray = 0
+
+	for (let i = 0; i < array.length; i++){
+			
+		mewArray += array[i]
+		
+	}
+
+	return mewArray/array.length
 }
+
